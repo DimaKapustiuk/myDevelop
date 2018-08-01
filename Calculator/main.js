074580ea99f9f4dbf97ddc347b27925f02674a18
Дмитрий Капустюк <dima.kapustiuk@gmail.com>
2018-08-02 00:01:14 +0300

    first commit

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,85 +1,26 @@
-function Calculate(num) {
-    this.num = num;
-    this.sum = function() {
-        let total = 0;
-        for (const value of this.num) {
-            total += value;
-        }
-        return total;
-    };
-    this.minus = function() {
-        let result = this.num[0];
-        
-        for (let i = 1; i < this.num.length; i += 1) {
-            result -= this.num[i];
-        }
-        
-        return result;
-    }
-
-    this.mult = function() {
-        let result = 1;
-
-        for (const value of this.num) {
-            result *= value;
-        }
-
-        return result;
-    };
-};
+/*
+  Напишите скрипт для создания списка ul.
+  
+  Для каждого пункта:
+    - Запрашивайте содержимое пункта li у пользователя с помощью prompt.
+    - Создавайте пункт и добавляйте его к ul.
+    - Процесс прерывается, когда пользователь нажимает Cancel.
+    - Все элементы списка должны создаваться динамически.
+*/
 
 
-let arr = [];
-const Calc = new Calculate(arr);
-let operator;
+const list = document.querySelector('.list');
 
 do {
-    const userInput = prompt("Введите числа для вычисления! К примеру если 2 - 1, введите сначала 2 потом 1 и = для окончания", '=');
-
-    if (userInput === null) {
-        alert("Отмена пользователем");
-        break;
-    } else if (userInput === "=") {
-    		changeOperation();
-        break;
+    const userInput = prompt ("Ведите по одному пункту в список и нажимайте ок!  Закончить: Отмена")
+    if(userInput !== null) {
+        list.innerHTML +=`<li> ${userInput} </li>`
     } else {
-        const userNumber = Number.parseFloat(userInput);
-        arr.push(userNumber);
+        break;
     }
 
-} while (true);
+} while (true)
 
-function changeOperation() {
-    const userOperation = prompt("Выберите операцию для ваших чисел! введите * - умножение, + - сумма, - разница");
-    if (userOperation === null) {
-        alert("Отмена пользователем!");
-    } else if (userOperation === "*") {
-    		operator = "*";
-    		console.log(operator);
-    		const result = Calc.mult();
-    		alert("Ваша операция " + massageArr(arr));
-        alert(`Результат умножения = ${result}`);
-    } else if (userOperation === "+") {
-    		operator = "+";
-        const result = Calc.sum();
-        alert("Ваша операция " + massageArr(arr));
-        alert(`Результат сложения = ${result}`);
-    } else if (userOperation === "-") {
-    		operator = "-";
-        const result = Calc.minus();
-        alert("Ваша операция " + massageArr(arr));
-        alert(`Результат сложения = ${result}`);
-    }
 
-    function massageArr (arr){
-    	let arrNum = arr;
-    	for (const value of arrNum) {
-    		const idx = arrNum.indexOf(value);
-    		if(idx % 2 === 1) {
-    			arrNum.splice(idx, 0, operator);
-    		}
-    	}
 
-    	return arrNum.join(" ");
-    }
-}
+
